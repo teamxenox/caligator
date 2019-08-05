d25ad8af22921d5c1d802e54fe4eb777dc6af645
rajesh <merajesh04@gmail.com>
2019-08-05 23:24:16 +0530

    Add exponent evaluation with unit test

--- utils/coreCalc.js
+++ utils/coreCalc.js
@@ -1,7 +1,15 @@
 "use strict";
 
 // set the operators to be used 
-const operators = ['+', '-', '*', '/', '%', '(', ')'];
+const operators = ['+', '-', '*', '/', '%', '(', ')', '**'];
+const precedence = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+    '%': 2,
+    '**': 3 
+};
 
 const evalExp = (expression) => {
     let tokens = [];
@@ -52,16 +60,26 @@ const getTokens = exp => {
     let operand = "";
 
     for(let i = 0; i < exp.length; i++) {
-        if(!operators.includes(exp[i])) {
-            operand += exp[i];
-        }else {
+        if(exp[i] === '*' && exp[i+1] === '*'){
             if(operand !== '') 
                 tokens.push(operand);
-            tokens.push(exp[i]);
-            operand = "";
+            operand = '';
+            tokens.push('**');
+        } else if(exp[i] === '*' && exp[i-1] === '*') {
+            continue;
+        } else {
+            if(!operators.includes(exp[i])) {
+                operand += exp[i];
+            }else {
+                if(operand !== '') 
+                    tokens.push(operand);
+                tokens.push(exp[i]);
+                operand = "";
+            }
         }
     }
     tokens.push(operand);
+
     // filter out any junk/spaces
     tokens = tokens.filter(token => token !== '');
     return tokens;
@@ -81,16 +99,18 @@ const operate = (operator, operand1, operand2) => {
         case '/':
             if(operand1 === 0) throw "Divide by Zero Exception Occurred !!"; 
             return operand2 / operand1;
+        case '**':
+            return Math.pow(operand2, operand1);
     }
 };
 
 const hasPrecedence = (op1, op2) => {
-    if(op2 === '(' || op2 === ')') {
-        return false;
-    }else if ((op1 === '*' || op1 === '/' || op1 === '%') && (op2 === '+' || op2 === '-')) {
-        return false;
-    }else {
+    if(op2 === '(' || op2 === ')') return false;
+
+    if(precedence[op2] >= precedence[op1]) {
         return true;
+    }else {
+        return false;
     }
 };
 
