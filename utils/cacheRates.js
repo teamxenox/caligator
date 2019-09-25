5f078f4d56172cb718d7912276c603bd8a9de755
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-09-25 15:21:44 +0530

    [update] switch to different rates api

--- utils/cacheRates.js
+++ utils/cacheRates.js
@@ -1,45 +1,40 @@
 module.exports = async function(config) {
+	// cache currency conversion rates
+	if (config.has('rates')) {
+		const h24ms = new Date().getTime() + 1 * 24 * 60 * 60 * 1000;
 
-        // cache currency conversion rates
-        if(config.has('rates')) {
-          const h24ms = new Date().getTime() + (1 * 24 * 60 * 60 * 1000);
+		/**
+		 * if rates are more than 24h old make sure to cache new Rates
+		 */
+		if (h24ms < config.get('last_cached')) {
+			await cacheRates();
+		}
+	} else {
+		/**
+		 * cache Rates if rates doesnt exist already
+		 * on app first run
+		 */
+		await cacheRates();
+	}
 
-          /**
-           * if rates are more than 24h old make sure to cache new Rates
-           */
-          if (h24ms < config.get('last_cached')) {
-              await cacheRates();
-          }
+	/**
+	 * function to get latest rates from openexchangerates and cache them
+	 * to electron store
+	 */
+	async function cacheRates() {
+		const axios = require('axios');
 
-      } else {
+		const response = await axios(
+			`https://api.exchangeratesapi.io/latest?base=USD`
+		);
 
-          /**
-           * cache Rates if rates doesnt exist already
-           * on app first run
-           */
-          await cacheRates();
-      }
+		if (response.status === 200) {
+			config.set({
+				rates: response.data.rates,
+				last_cached: new Date()
+			});
+		}
 
-      /**
-       * function to get latest rates from openexchangerates and cache them
-       * to electron store
-       */
-      async function cacheRates() {
-          const axios = require('axios');
-
-          const appId = 'ee2f6479bd2d4938b47ccda233d14c55';
-
-          const response = await axios(`https://openexchangerates.org/api/latest.json?app_id=${appId}`);
-
-          if(response.status === 200) {
-
-              config.set({
-                  rates: response.data.rates,
-                  last_cached: new Date()
-              });
-
-          };
-
-          return;
-      };
-}
+		return;
+	}
+};
