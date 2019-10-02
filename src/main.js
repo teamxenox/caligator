ff5fdd3be0b2ea1cd3e46cfc8a243d3ae7a53c12
Kiser360 <Kiser360@gmail.com>
2019-10-01 21:10:51 -0500

    Add a simple and light web framework: v2.svelte.dev

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import App from './App.html';
+
+const app = new App({
+	target: document.body,
+});
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+	entry: {
+		bundle: ['./src/main.js']
+	},
+	resolve: {
+		extensions: ['.js', '.html']
+	},
+	output: {
+		path: __dirname + '/public',
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						skipIntroByDefault: true,
+						nestedTransitions: true,
+						emitCss: true,
+						hotReload: true
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					/**
+					 * MiniCssExtractPlugin doesn't support HMR.
+					 * For developing, use 'style-loader' instead.
+					 * */
+					prod ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+	mode,
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
+	devtool: prod ? false: 'source-map'
+};
