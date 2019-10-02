3976310ed2ab21d4f88a5f0e6cbff88f0a75145e
Kiser360 <Kiser360@gmail.com>
2019-10-02 00:11:53 -0500

    Cache icons for offline capability

--- src/service.js
+++ src/service.js
@@ -6,9 +6,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     { url: "./bundle.css", revision },
     { url: "./bundle.js", revision },
     { url: "./index.html", revision },
-    { url: "./assets/close.svg", revision },
-    { url: "./assets/menu.svg", revision },
+    { url: "./index.html", revision },
     { url: "./assets/OpenSans.woff", revision },
+    { url: './assets/icons/icon-72x72.png', revision },
+    { url: './assets/icons/icon-96x96.png', revision },
+    { url: './assets/icons/icon-128x128.png', revision },
+    { url: './assets/icons/icon-144x144.png', revision },
+    { url: './assets/icons/icon-152x152.png', revision },
+    { url: './assets/icons/icon-192x192.png', revision },
+    { url: './assets/icons/icon-384x384.png', revision },
+    { url: './assets/icons/icon-512x512.png', revision },
 ]);
 
 precaching.precacheAndRoute(self.__precacheManifest || [])
