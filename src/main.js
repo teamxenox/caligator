29dd7399b1132ec2790c103bf19c6d10599677e2
Kiser360 <Kiser360@gmail.com>
2019-10-02 21:47:50 -0500

    Explicitly catch install prompt event, maybe fix lack of install on mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import App from './App.html';
 import { checkForSwSupport, registerServiceWorker } from './tools';
 import * as cacheRates from '../utils/cacheRates';
+import { AppStore } from './App.store';
 
 const app = new App({
 	target: document.body,
@@ -16,5 +17,13 @@ if (!window.dev) {
 }
 
 cacheRates();
+
+window.addEventListener('beforeinstallprompt ', e => {
+	AppStore.set({ installPrompt: e });
+});
+
+window.addEventListener('appinstalled', e => {
+	console.log('window.appinstalled', e);
+});
 
 export default app;
